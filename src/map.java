5a25fe19a883f9443af3f5fd47ea29c3b15f61a4
mine <elmregl.gmailgit>
2021-03-05 18:20:12 +0100

    classe map plus tableau 5*8

--- src/map.java
+++ src/map.java
@@ -3,6 +3,6 @@ public class map {
 	public static void main(String[] args) {
 		int[][] matrice=new int[5][];
 		for (int i=0 ; i<matrice.length; i++)
-		    matrice[i]=new int[6];
+		    matrice[i]=new int[8];
 	}
 }
