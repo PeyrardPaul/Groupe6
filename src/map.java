43f82e9779b55736dddc2b4213700ca8d0b8f4b5
paulp <paulp@LAPTOP-452DAO5V.home>
2021-03-05 18:49:36 +0100

    Erreur paul

--- src/map.java
+++ src/map.java
@@ -1,8 +1,8 @@
 
 public class map {
 	public static void main(String[] args) {
-		int[][] matrice=new int[5][];
-		for (int i=0 ; i<matrice.length; i++)
-		    matrice[i]=new int[6];
+		int[][] matrice = new int[5][];
+		for (int i = 0; i < matrice.length; i++)
+			matrice[i] = new int[6];
 	}
 }
