4c14cb107c8827af3ed5540023457c8cbc5d8118
paulp <paulp@192.168.1.33>
2021-03-06 22:44:25 +0100

    Création méthode potion et piege dans classe personnage

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,24 +1,23 @@
 
 public class Personnage {
 	private String name = "";
-	private int vie = 10;
+	private int pv = 10;
 	private final int numerojoueur;
 	public static int nbjoueur = 0;
 
-	public Personnage(String name, int vie) throws PersonnageException {
+	public Personnage(String name, int pv) throws PersonnageException {
 		setName(name);
-		setVie(vie);
-		numerojoueur = ++nbjoueur;	
+		setPv(pv);
+		numerojoueur = ++nbjoueur;
 	}
 
-	
 	public int getNumerojoueur() {
 		return numerojoueur;
 	}
 
 	public String getName() {
 		if (name == "")
-			name = "Player"+String.valueOf(getNumerojoueur());
+			name = "Player" + String.valueOf(getNumerojoueur());
 		return name;
 	}
 
@@ -28,26 +27,40 @@ public class Personnage {
 		this.name = name;
 	}
 
-	public int getVie() {
-		if (vie != 10)
-			vie = 10;
-		return vie;
+	public int getPv() {
+		if (pv != 10)
+			pv = 10;
+		return pv;
 	}
 
-	public void setVie(int vie) throws PersonnageException {
-		if (getVie()!= 10)
+	public void setPv(int pv) throws PersonnageException {
+		if (getPv() != 10)
 			throw new PersonnageException("Attention, vous n'avez pas autant de vie qu'il ne le faut");
-		this.vie = vie;
+		this.pv = pv;
 	}
 
 	public String toString() {
-		return "Vous vous appelez " + getName() + " et vous commencez avec " + getVie() + " de vie";
+		return "Vous vous appelez " + getName() + " et vous commencez avec " + getPv() + " de vie";
+	}
+
+	public String potion() {
+		System.out.println("Vous avez trouvé une potion : +2 PV");
+		pv = pv + 2;
+		return "Vous avez donc maintenant " + pv + "PV";
+	}
+
+	public String piege() {
+		System.out.println("Vous êtes tombés sur un piège : -3 PV");
+		pv = pv - 3;
+		return "Vous avez donc maintenant " + pv + "PV";
 	}
 
 	public static void main(String[] args) throws PersonnageException {
-		Personnage un = new Personnage("Pierre", 5);
+		Personnage un = new Personnage("Pierre", 10);
 		System.out.println(un);
-		Personnage deux = new Personnage("Marie", 18);
+		Personnage deux = new Personnage("Marie", 10);
 		System.out.println(deux);
+		System.out.println(deux.potion());
+		System.out.println(deux.piege());
 	}
 }
