3961ae0a24ba1db442ea3025f840709383c3c005
paulp <paulp@192.168.1.33>
2021-03-08 01:13:57 +0100

    modifs

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@
 public class Main {
 	public static void main(String[] args){
 		Map map = new Map();
-		Personnage perso = new Personnage();
 	}
 }
--- src/Map.java
+++ src/Map.java
@@ -1,18 +1,45 @@
+import java.util.Random;
 
 public class Map {
 
-	public Map() { //Je me suis aidé de cette vidéo-->  https://youtu.be/QVXM9YeO7rw
+	private String[][] map;
+
+	public Map() { // Je me suis aidé de cette vidéo--> https://youtu.be/QVXM9YeO7rw
 		System.out.println("Voici la carte ---->");
-		String[][] map = new String[15][15];
+		map = new String[15][15];
 		for (int i = 0; i < 15; i++) {
 			for (int j = 0; j < 15; j++) {
 				map[i][j] = "-";
-				if (map[7][7] == "-") {
-					map[7][7] = "P";
-				}
+				map[7][7] = "P";
 				System.out.print(" " + map[i][j]);
 			}
 			System.out.println(" ");
+			/*if (i != 6) {
+				Random rand = new Random();
+				map[rand.nextInt(14)][rand.nextInt(14)] = "M";
+			}*/
+		}
+	}
+
+	public void objet(int a, int b, String c) { // a etant les lignes et b les colonnes
+		if (a < 0 || b < 0 || a > 14 || b > 14) {
+			System.out.println("Attention, vous voulez mettre un objet en dehors de la carte");
+		} else {
+			if (map[a][b] == "-") {
+				map[a][b] = c;
+			} else {
+				System.out.println("Attention, l'emplacement est déja pris!");
+			}
+		}
+	}
+
+	public void carte() {
+		for (int i = 0; i < 15; i++) {
+			for (int j = 0; j < 15; j++) {
+				map[i][j] = "-";
+				System.out.print(" " + map[i][j]);
+			}
+			System.out.print(" ");
 		}
 	}
 }
