7682c3615f1cf27c4d1efa9b6fcc8cad04f80a93
paulp <paul.peyrard17@gmail.com>
2021-04-16 18:38:05 +0200

    salut c'est paul 2

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,7 @@ public class Main {
 		 * System.out.print(aleap+"  "); p+=1; } }
 		 */
 		Map map = new Map();
-<<<<<<< HEAD
-=======
 		//System.out.println(map.getMap());
->>>>>>> refs/remotes/origin/main
 		Personnage perso = new Personnage();
 		// map.casesPieges(perso,map);
 		// map.deplacerMultiple(perso, map, 3);
